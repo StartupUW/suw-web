ebf75edcd7128ed5da4ff68849a88b55e0b4f1ba
Daniel Fang <danfang@uw.edu>
2015-08-24 06:36:13 +0000

    Adding prod settings

--- web/settings.py
+++ web/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test',
+        'NAME': 'dev',
         'USER': 'web',
         'PASSWORD': '+k1UnULgChRj+HFCekIu',
         'HOST': 'localhost',
