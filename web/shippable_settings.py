c44f30c75e0b7bfde9b3a34ad30e0bce980b8949
Daniel Fang <danfang@uw.edu>
2015-08-24 06:42:18 +0000

    Trying out env

---
+++ web/shippable_settings.py
@@ -0,0 +1,12 @@
+from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
