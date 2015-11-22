d29ec3d835072e247e6baa2e8ae610ae52a58944
Ayush Saraf <ayush29f@uw.edu>
2015-11-22 07:35:51 +0000

    Scrapes the events to the database

--- scripts/fb.py
+++ scripts/fb.py
@@ -1,4 +1,6 @@
 import requests
+import sqlalchemy
+from sqlalchemy import *
 
 # APP CONSTANTS
 CLIENT_ID = '912240588829356'
@@ -14,4 +16,35 @@ data = r.json()['data']
 for event in data:
 	print event['name']
 
+engine = create_engine('postgresql+psycopg2://postgres:asdfasdf@localhost/suw')
+conn = engine.connect()
+
+metadata  = MetaData()
+events = Table('events', metadata,
+	Column('id', BigInteger, primary_key=True),
+	Column('name', String, nullable=False),
+	Column('desc', String),
+	Column('place', String, nullable=False),
+	Column('lat', Numeric, nullable=True),
+	Column('lng', Numeric, nullable=True),
+	Column('start', DateTime, nullable=False),
+	Column('end', DateTime, nullable=True)
+)
+metadata.create_all(engine)
+
+for event in data:
+	events_data = {
+		'id' : event['id'],
+		'name' : event['name'],
+		'desc' : event['description'],
+		'place' : event['place']['name'],
+		'start' : event['start_time'],
+	}
+	if('end_time' in event):
+		events_data['end'] = event['end_time']	
+	if('location' in event['place']):
+		events_data['lat'] = event['place']['location']['latitude']
+		events_data['lng'] = event['place']['location']['longitude']
+
+	conn.execute(events.insert(), events_data)
 
