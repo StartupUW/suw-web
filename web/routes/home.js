aaa3e1e852709cbc0f8ef6986c6028539707f388
Daniel Fang <danfang@cs.washington.edu>
2015-11-23 15:06:46 -0800

    Fleshing out events template

--- web/routes/home.js
+++ web/routes/home.js
@@ -1,5 +1,20 @@
 // Home Page
 
+var mockEvents = [
+  {
+    name: 'Event 1',
+    start: 'Jan 1, 1970',
+    end: 'Jan 2, 1970',
+    description: 'This is an event description!'
+  },
+  {
+    name: 'Event 1',
+    start: 'Jan 1, 1970',
+    end: 'Jan 2, 1970',
+    description: 'This is an event description!'
+  }
+];
+
 module.exports = function(req, res, next) {
-    res.render('home', { title: 'Startup UW' });
+    res.render('home', { title: 'Startup UW', events: mockEvents });
 };
