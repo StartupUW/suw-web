083987f02d239ca8744cce07a1348f4767366d6c
Ayush Saraf <ayush29f@uw.edu>
2015-12-24 08:55:47 +0000

    Very basic version of fb events script. Drops table each time

--- scripts/fb.py
+++ scripts/fb.py
@@ -11,8 +11,15 @@ r = requests.get('https://graph.facebook.com/oauth/access_token?client_id=' + CL
 token = r.text.split('=')[1]
 
 # Get the data from the facebook api
-r = requests.get('https://graph.facebook.com/StartupUW/events?access_token=' + token)
-data = r.json()['data']
+url = 'https://graph.facebook.com/StartupUW/events?access_token=' + token
+data = []
+while(url != None):
+	r = requests.get(url)
+	data.extend(r.json()['data'])
+	if('next' in r.json()['paging']):
+		url = r.json()['paging']['next']
+	else:
+		url = None
 
 # Start a connection
 engine = create_engine('postgresql+psycopg2://postgres:asdfasdf@localhost/suw')
@@ -24,29 +31,39 @@ events = Table('events', metadata,
 	Column('id', BigInteger, primary_key=True),
 	Column('name', String, nullable=False),
 	Column('desc', String),
-	Column('place', String, nullable=False),
+	Column('place', String, nullable=True),
 	Column('lat', Numeric, nullable=True),
 	Column('lng', Numeric, nullable=True),
 	Column('start_time', DateTime, nullable=False),
 	Column('end_time', DateTime, nullable=True),
 	Column('timestamp', TIMESTAMP(timezone=True), default=func.now())
 )
+events.drop(engine, checkfirst=True)
 metadata.create_all(engine)
 
 # Get every event and add it to the database
+events_data = []
 for event in data:
-	events_data = {
+	event_data = {
 		'id' : event['id'],
 		'name' : event['name'],
 		'desc' : event['description'],
-		'place' : event['place']['name'],
+		'place' : None,
 		'start_time' : event['start_time'],
+		'end_time' : None,
+		'lat' : None,
+		'lng' : None
 	}
 	if('end_time' in event):
-		events_data['end_time'] = event['end_time']	
-	if('location' in event['place']):
-		events_data['lat'] = event['place']['location']['latitude']
-		events_data['lng'] = event['place']['location']['longitude']
+		event_data['end_time'] = event['end_time']	
+	if('place' in event):
+		if('location' in event['place']):
+			event_data['lat'] = event['place']['location']['latitude']
+			event_data['lng'] = event['place']['location']['longitude']
+		if('name' in event['place']):
+			event_data['place'] = event['place']['name']
+	
+	events_data.append(event_data)
 
-	conn.execute(events.insert(), events_data)
+conn.execute(events.insert(), events_data)
 
