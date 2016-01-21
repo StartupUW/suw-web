aeba3f2e0fd90b65ff2d1027d821792ca4551050
Daniel Fang <danfang@uw.edu>
2016-01-21 03:10:11 -0800

    Fixing env variable name

--- routes/database.js
+++ routes/database.js
@@ -1,7 +1,7 @@
 var Sequelize = require('sequelize');
 
 var user = process.env.PG_USER;
-var password = process.env.PG_PASSOWRD;
+var password = process.env.PG_PASSWORD;
 var database = process.env.PG_DATABASE || 'suw';
 
 var connStr = 'postgres://' + user + ':' + password + '@localhost:5432/' + database;
