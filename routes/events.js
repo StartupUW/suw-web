03c1024f565e6ce0973b2b21041e458719a6cd1b
Divye Pratap Jain <dpjain@uw.edu>
2016-05-10 19:19:56 -0700

    started pagenation

--- routes/events.js
+++ routes/events.js
@@ -4,7 +4,11 @@ var moment = require('moment');
 var db = require('./database.js');
 
 module.exports = function(req, res, next) {
-  db.query('SELECT * FROM events ORDER BY start_time DESC', { type: db.QueryTypes.SELECT }).then(function(data) {
+  console.log(req.path)
+  var no = req.path.split("/");
+  var num = no[no.length - 1];
+  var offset = 5*num;
+  db.query('SELECT * FROM events ORDER BY start_time DESC' + ' LIMIT 5 OFFSET ' + offset, { type: db.QueryTypes.SELECT }).then(function(data) {
     data = data.map(function(event) {
       event.at = moment(event.start_time).format('MMMM Do YYYY, h:mm a');
       return event;
