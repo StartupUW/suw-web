3505de309142cfce722aae6d837935f8946fc795
Divye Pratap Jain <dpjain@uw.edu>
2016-05-20 00:10:52 -0700

    minor edit in page indexing

--- routes/events.js
+++ routes/events.js
@@ -5,8 +5,10 @@ var db = require('./database.js');
 
 module.exports = function(req, res, next) {
   var no = req.params.page;
+  if (no == 0){
+    no = 1;
+  }
   var offset = 5*(no - 1);
-  console.log(no)
   db.query('SELECT * FROM events ORDER BY start_time DESC' + ' LIMIT 10 OFFSET ' + offset, { type: db.QueryTypes.SELECT }).then(function(data) {
     data = data.map(function(event) {
       event.at = moment(event.start_time).format('MMMM Do YYYY, h:mm a');
