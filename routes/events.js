7d70d78370c35f23fdc84c6079c2a406e0a7f07c
Divye Pratap Jain <dpjain@uw.edu>
2016-05-19 21:29:16 -0700

    Pagination Compelte

--- routes/events.js
+++ routes/events.js
@@ -5,13 +5,13 @@ var db = require('./database.js');
 
 module.exports = function(req, res, next) {
   var no = req.params.page;
-  var offset = 5*no;
+  var offset = 5*(no - 1);
   console.log(no)
-  db.query('SELECT * FROM events ORDER BY start_time DESC' + ' LIMIT 5 OFFSET ' + offset, { type: db.QueryTypes.SELECT }).then(function(data) {
+  db.query('SELECT * FROM events ORDER BY start_time DESC' + ' LIMIT 10 OFFSET ' + offset, { type: db.QueryTypes.SELECT }).then(function(data) {
     data = data.map(function(event) {
       event.at = moment(event.start_time).format('MMMM Do YYYY, h:mm a');
       return event;
     });
-    res.render('events', {  title: 'Events' , events: data,  page:parseInt(no)});
+    res.render('events', {  title: 'Events' , events: data.slice(0, 5),  page: parseInt(no), total: data.length});
   });
 };
