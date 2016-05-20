325bc89da7582e1b8fc11c148017aef495d2a076
Daniel Fang <danfang@uw.edu>
2016-05-20 10:14:37 -0700

    Touch up events page, revert to standard homepage

--- routes/events.js
+++ routes/events.js
@@ -3,17 +3,31 @@
 var moment = require('moment');
 var db = require('./database.js');
 
-module.exports = function(req, res, next) {
-  var no = req.params.page;
-  if (no == 0){
-    no = 1;
-  }
-  var offset = 5*(no - 1);
-  db.query('SELECT * FROM events ORDER BY start_time DESC' + ' LIMIT 10 OFFSET ' + offset, { type: db.QueryTypes.SELECT }).then(function(data) {
-    data = data.map(function(event) {
-      event.at = moment(event.start_time).format('MMMM Do YYYY, h:mm a');
-      return event;
+var PAGE_LIMIT = 5;
+
+module.exports = function(req, res) {
+  var page = parseInt(req.params.page || 1);
+  var offset = PAGE_LIMIT * (page - 1);
+  var total = 'SELECT COUNT(*) FROM events';
+  db.query(total, { type: db.QueryTypes.SELECT }).then(function(data) {
+    var count = data[0].count;
+    var query = 'SELECT * FROM events ORDER BY start_time DESC' + ' LIMIT ' + PAGE_LIMIT + ' OFFSET ' + offset;
+    db.query(query, { type: db.QueryTypes.SELECT }).then(function(data) {
+      if (data.length == 0) {
+        res.render('error', { message: 'Not found' });
+        return;
+      }
+      var events = data.map(function(event) {
+        event.at = moment(event.start_time).format('MMMM Do YYYY, h:mm a');
+        return event;
+      });
+      res.render('events', {
+        title: 'Events',
+        events: events,
+        numEvents: count,
+        PAGE_LIMIT: PAGE_LIMIT,
+        page: page,
+      });
     });
-    res.render('events', {  title: 'Events' , events: data.slice(0, 5),  page: parseInt(no), total: data.length});
   });
 };
