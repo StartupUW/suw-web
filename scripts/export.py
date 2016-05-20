a2a8b5ca345f39e4b6f34c98a4dc8fd6fc812dbe
Divye Pratap Jain <dpjain@uw.edu>
2016-05-19 21:32:07 -0700

    minor edits

--- scripts/export.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.putenv("PG_USER","web")
-os.putenv("PG_PASSWORD","TBtsAH24tfUMek7Q==")
-os.putenv("FB_CLIENT_ID","912240588829356")
-os.putenv("FB_CLIENT_SECRET","cf8d4abe8b14cdc7d03f1a23f9abcbe6")
-
-os.system("python scripts/fb.py")
